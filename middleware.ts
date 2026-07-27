fa7f896b11369dd378de8548abebca895cf16889
Asik Ali <asik14923@gmail.com>
2026-07-27 15:40:17 +0530

    A

--- middleware.ts
+++ middleware.ts
@@ -1,25 +1,4 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
-import { NextResponse } from 'next/server';
-
-const protectedRoute = createRouteMatcher([
-  '/',
-  '/upcoming',
-  '/meeting(.*)',
-  '/previous',
-  '/recordings',
-  '/personal-room',
-]);
-
-export default clerkMiddleware((auth, req) => {
-  if (protectedRoute(req)) {
-    const { userId } = (auth() as any);
-    if (!userId) {
-      const signInUrl = new URL('/sign-in', req.url);
-      signInUrl.searchParams.set('redirect_url', req.url);
-      return NextResponse.redirect(signInUrl);
-    }
-  }
-});
+ 
 
 // ✅ This catches everything except static assets
 export const config = {
